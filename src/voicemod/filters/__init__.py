c54da68495d2043e555f5d64410d6ad5d421d976
Eden Ernandes <eden.santana@neurotech.com.br>
2021-04-04 22:24:42 -0300

    Add new filters to module

--- src/voicemod/filters/__init__.py
+++ src/voicemod/filters/__init__.py
@@ -1,3 +1,5 @@
 from voicemod.filters.volume import VolumeFilter
 from voicemod.filters.reverb import ReverbFilter
 from voicemod.filters.pitch_shifter import PitchShiftFilter
+from voicemod.filters.whale import WhaleFilter
+from voicemod.filters.reverse import ReverseFilter
